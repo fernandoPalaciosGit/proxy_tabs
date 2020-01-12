d47ac705b71eb686aa46d3baad9aee6f1dad94d0
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2020-01-12 14:48:45 +0100

    refactor handler events file, add jquery dependency

--- example/parent_tab.js
+++ example/parent_tab.js
@@ -1,10 +1,28 @@
-import bindTabEvents from './multitab_events';
+import ProxyTabs from "proxy_tabs";
 
 const WINDOW_CONTEXT_NAME = 'My-app';
 const TAB_CONTEXT_NAME = 'My-second-app';
+const proxyTabs = new ProxyTabs(window, WINDOW_CONTEXT_NAME);
+const EVENT_HANDLER = {
+    'childTabCounter': ({originalEvent = {}}) => {
+        const {tabCount, tabName} = originalEvent.source;
+
+        if (_.isNumber(tabCount) && _.isString(tabName)) {
+            console.info(`Another tab has opened: ${{tabCount, tabName}.toString()}`);
+        }
+    },
+    'childTabThrowsError': ({originalEvent = {}}) => {
+        const {error} = originalEvent.data;
+
+        if (!!error) {
+            console.error(`Some errors happens in the actual tab: ${error.toString()}`);
+        }
+    },
+};
+
 
 // create proxy handlers to open communication with tabs
-bindTabEvents(WINDOW_CONTEXT_NAME);
+proxyTabs.setProxyEvents(EVENT_HANDLER).bindHandlers();
 // create new tab context
 const childTab = window.open('URL', TAB_CONTEXT_NAME);
 // add metadata to the new tab to have some data reference to parent tab
--- proxy_tabs.js
+++ proxy_tabs.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 const COMMUNICATE_TABS = 'message';
 
 class ProxyTabs {
